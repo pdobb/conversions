f164da8a8face037105885b7edb9d281727b2b38
Paul Dobbins <pdobbins@gmail.com>
2014-08-31 15:07:09 -0400

    Remove development dependency on `wrong` gem.

--- test/boolean_test.rb
+++ test/boolean_test.rb
@@ -6,13 +6,13 @@ describe Conversions do
   describe ".Boolean" do
     it "returns true for truthy-looking values" do
       [true, 1, '1', 't', 'T', 'true', 'TRUE'].each do |value|
-        assert { Boolean(value) }
+        assert Boolean(value), "Check `#{value}`"
       end
     end
 
     it "returns false for non-truthy-looking values" do
       [false, 0, '0', 'f', 'F', 'false', 'FALSE', '', nil].each do |value|
-        deny { Boolean(value) }
+        refute Boolean(value), "Check `#{value}`"
       end
     end
   end
